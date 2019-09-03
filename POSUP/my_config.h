c4649b4b59345b8d7a6d2216f5123cd0fb3a7be6
Thang Hoang <hoangmin@oregonstate.edu>
2019-09-03 13:58:15 -0400

    Add files via upload

--- POSUP/my_config.h
+++ POSUP/my_config.h
@@ -2,7 +2,7 @@
 const string PEER_ADDRESS = "tcp://127.0.0.1:5555";
 
 //=== PATHS ==================================================================
-const std::string rootPath = "/data/data/";
+const std::string rootPath = "C:/data/";
 
 //const std::string clientLocalDir = rootPath + "client_local/";
 const std::string clientDataDir = rootPath + "client/";
@@ -12,8 +12,8 @@ const std::string FileORAMPath = rootPath + "server/file/";
 const std::string IdxBlockPath = rootPath + "idxBlock/";
 const std::string FileBlockPath = rootPath + "fileBlock/";
 
-//const std::string dbPath = rootPath + "db/";
-const std::string dbPath = "/home/hmthang/Documents/output_text/";
+const std::string dbPath = rootPath + "db/";
+//const std::string dbPath = "/home/hmthang/Documents/output_text/";
 
 const std::string metaPath = "meta/";
 const std::string bucketPath = "bucket/";
@@ -21,5 +21,8 @@ const std::string stashPath = "stash/";
 
 
 const std::string filename_kwBlockIDMap = "kw_map";
+const std::string filename_emptyIdxBlock = "empty_idx_block";
+const std::string filename_emptyIdxBlock_len = "empty_idx_block_len";
 const std::string filename_fileBlockIDMap = "file_map";
 const std::string filename_invertedIdx = "invertedIdx";
+
